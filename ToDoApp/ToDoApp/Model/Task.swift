7610711112849b7f6b0ed21f0ea99e4d838e3b55
AndreyPrez <aw06.1994@yandex.ru>
2022-04-08 11:17:03 +0300

    Task model is created and tested

--- ToDoApp/ToDoApp/Model/Task.swift
+++ ToDoApp/ToDoApp/Model/Task.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct Task {
+    var title: String
+    var description: String?
+    private(set) var date: Date?
+    var location: Location?
+    
+    init(title: String, description: String? = nil, location: Location? = nil) {
+        self.title = title
+        self.description = description
+        self.date = Date()
+        self.location = location
+        
+    }
+}
--- ToDoApp/ToDoAppTests/Model/TaskTests.swift
+++ ToDoApp/ToDoAppTests/Model/TaskTests.swift
@@ -6,30 +6,65 @@
 //
 
 import XCTest
+@testable import ToDoApp
+
 
 class TaskTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    // Red Gren Refactor
+   
+    // Task lable initialization test
+   func testInitTaskWithTitle() {
+       // given
+       let task = Task(title: "Foo")
+       // when
+       // then
+       XCTAssertNotNil(task)
+   }
+   
+   // Can we make a task with title and description
+   func testInitTaskWithTitleAndDescription() {
+       // given
+       let task = Task(title: "Foo", description: "Bar")
+       // when
+       // then
+       XCTAssertNotNil(task)
+   }
+   
+   func testWhenGivenTitleSetsTitle() {
+       
+       let task = Task(title: "Foo")
+       
+       
+       XCTAssertEqual(task.title, "Foo")
+   }
+   
+   func testWhenGivenDescriptionSetsDescription() {
+       
+       let task = Task(title: "Bar", description: "this is a Foo")
+       
+       XCTAssertTrue(task.description == "this is a Foo")
+   }
+   
+   
+   func testTaskInitsDate() {
+       
+       let task = Task(title: "Baz")
+       
+       XCTAssertNotNil(task.date)
+   }
+   
+   
+   func testWhenGivenLocationSetsLocation() {
+       
+       let location = Location(name: "Foo")
+       
+       let task = Task(title: "Bar", description: "Baz", location: location)
+       
+       XCTAssertEqual(location, task.location)
+   }
+   
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+   
 
 }
