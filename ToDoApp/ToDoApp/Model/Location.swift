f825b1d3c0a54de94e366a61106a74e9674f3708
AndreyPrez <aw06.1994@yandex.ru>
2022-04-08 11:05:25 +0300

    Task, Location models and TaskTest, LocationTest files added

---
+++ ToDoApp/ToDoApp/Model/Location.swift
@@ -0,0 +1,8 @@
+//
+//  Location.swift
+//  ToDoApp
+//
+//  Created by Андрей  on 08.04.2022.
+//
+
+import Foundation
---
+++ ToDoApp/ToDoApp/Model/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  ToDoApp
+//
+//  Created by Андрей  on 08.04.2022.
+//
+
+import Foundation
