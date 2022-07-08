22757fe69e94152c98fa023009017a667fdb0738
AndreyPrez <aw06.1994@yandex.ru>
2022-07-09 00:25:55 +0300

    Done & Undone button name tests

--- ToDoApp/ToDoApp/Controller/DataProvider.swift
+++ ToDoApp/ToDoApp/Controller/DataProvider.swift
@@ -20,7 +20,17 @@ class DataProvider: NSObject {
 }
 
 extension DataProvider: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+        
+        guard let section = Section(rawValue: indexPath.section) else { fatalError() }
+        
+        switch section {
+        case .toDo:
+            return "Done"
+        case .done:
+            return "Undone"
+        }
+    }
 }
 
 
