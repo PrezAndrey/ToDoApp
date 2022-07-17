390030db30d633d8b1254bfd69cc3ba4463c7bf2
AndreyPrez <aw06.1994@yandex.ru>
2022-07-18 02:18:38 +0300

    DetailViewController Elements tests

---
+++ ToDoApp/ToDoApp/Controller/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  ToDoApp
+//
+//  Created by Андрей  on 18.07.2022.
+//
+
+
+import UIKit
+import MapKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var mapView: MKMapView!
+}
